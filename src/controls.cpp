a34f1c90db8988cf0d8f9bedca6a0c3cb5e6d3a9
bradywarner <bradywarner27@gmail.com>
2026-02-12 21:23:55 -0500

    Changed Naming Scheme

--- src/controls.cpp
+++ src/controls.cpp
@@ -2,9 +2,9 @@
 
 pros::controller_digital_e_t WillScheme(Action action){
     switch (action){
-    case Action::TOPINTAKE:
-        return pros::E_CONTROLLER_DIGITAL_R1;
     case Action::OUTTAKE:
+        return pros::E_CONTROLLER_DIGITAL_R1;
+    case Action::REVERSE:
         return pros::E_CONTROLLER_DIGITAL_R2;
     case Action::FLAP:
         return pros::E_CONTROLLER_DIGITAL_L1;
@@ -12,7 +12,7 @@ pros::controller_digital_e_t WillScheme(Action action){
         return pros::E_CONTROLLER_DIGITAL_L2;
     case Action::SCRAPER:
         return pros::E_CONTROLLER_DIGITAL_X;
-    case Action::BOTTOMINTAKE:
+    case Action::INTAKE:
         return pros::E_CONTROLLER_DIGITAL_Y;
     case Action::WING:
         return pros::E_CONTROLLER_DIGITAL_A;
@@ -34,9 +34,9 @@ pros::controller_digital_e_t WillScheme(Action action){
 
 pros::controller_digital_e_t DavidScheme(Action action){
     switch (action){
-    case Action::TOPINTAKE:
-        return pros::E_CONTROLLER_DIGITAL_R2;
     case Action::OUTTAKE:
+        return pros::E_CONTROLLER_DIGITAL_R2;
+    case Action::REVERSE:
         return pros::E_CONTROLLER_DIGITAL_L2;
     case Action::FLAP:
         return pros::E_CONTROLLER_DIGITAL_Y;
@@ -44,7 +44,7 @@ pros::controller_digital_e_t DavidScheme(Action action){
         return pros::E_CONTROLLER_DIGITAL_A;
     case Action::SCRAPER:
         return pros::E_CONTROLLER_DIGITAL_B;
-    case Action::BOTTOMINTAKE:
+    case Action::INTAKE:
         return pros::E_CONTROLLER_DIGITAL_R1;
     case Action::WING:
         return pros::E_CONTROLLER_DIGITAL_X;
